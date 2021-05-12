0075ba73acb4962f455dbe45c312006827ade1ad
DieG02 <43993554@alejandrovolta.edu.ar>
2021-05-12 17:16:14 -0300

    Get orders by busnisessId works! (req.body.bussinessId)

--- Server/Dev/order.ts
+++ Server/Dev/order.ts
@@ -1,10 +1,60 @@
 import express from "express";
 import Order from "../Models/Order";
+import Product from "../Models/Product";
 import ProductInCart from "../Models/ProductInCart";
-import ProductInCart from "../Models/ProductInCart";
+import ProductInOrder from "../Models/ProductInOrder";
 import isBusiness from "../Api/middleware/isBusiness";
 
+
 const router = express.Router();
+
+// Add a new order with userId = 1
+router.post("/", async (req, res) => {
+  await Order.create({
+    userId: req.body.userId,
+    state: "completed"
+  });
+  res.sendStatus(200)
+})
+
+// Add product to Order
+router.post("/product", async (req, res) => {
+  const { userId, productId } = req.body;
+  const order = await Order.findOne({ where: { userId } });
+  await ProductInOrder.create({
+    amount: 10,
+    price: 375,
+    orderId: order!.id,
+    productId
+  });
+  res.sendStatus(200);
+});
+
+
+// Get orders by businessId 
+router.get("/", async (req, res) => {
+  const { businessId } = req.body // --> req.business.id
+
+  const myProducts = await Product.findAll({
+    where: { businessId: businessId }
+  })
+  const promisesArr = myProducts.map(async ({ id }) => {
+    return await ProductInOrder.findOne({
+      where: { productId: id }
+    })
+  })
+
+  setTimeout(() => {
+    try {
+      res.json(Promise.all(promisesArr))
+    } catch(error) {
+      console.log('error:', error);
+    }
+  }, 1000)
+})
+
+
+
 
 router.put("/product", async (req, res) => {
   const { userId, productId } = req.body;
@@ -17,13 +67,5 @@ router.put("/product", async (req, res) => {
   });
   res.sendStatus(200);
 });
-
-router.get("/orders", isBusiness, async (req, res) => {
-  res.json(
-    await req.business!.$get("orders", {
-      include: [{ model: ProductInCart }]
-    })
-  );
-})
 
 export default router;
