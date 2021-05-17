1e1c7ad9b5b5e73a50e19eb7d6fa0c807307564b
DieG02 <43993554@alejandrovolta.edu.ar>
2021-05-17 14:42:42 -0300

    order detail

--- Server/Api/Routes/business/orders/getMyOrders.ts
+++ Server/Api/Routes/business/orders/getMyOrders.ts
@@ -1,34 +1,21 @@
 import { Router } from "express";
 import Product from "../../../../Models/Product";
-import ProductInOrder from "../../../../Models/ProductInOrder";
+import Order from "../../../../Models/Order";
 import isBusiness from "../../../middleware/isBusiness";
 
 const ROUTE = "/business/orders/";
 
-// Get orders by businessId 
 export default Router({ mergeParams: true }).get(
   ROUTE,
   isBusiness,
   async (req, res) => {
-    const myProducts = await Product.findAll({
-      where: { businessId:  req.business.id }
-    })
-    
-    const promisesArr = myProducts.map(async ({ id }) => {
-      return await ProductInOrder.findOne({
-        where: { productId: id }
-      })
-    })
-
-    setTimeout(() => {
-      async function orders() {
-        try {
-          res.json(await Promise.all(promisesArr))
-        } catch(error) {
-          console.log('error:', error);
-        }
+   const result = await Order.findAll({
+      where: { state: "completed" },
+      include: {
+        model: Product,
+        where: { businessId: req.business.id } 
       }
-      orders()
-    }, 1000)
+    })
+    res.json(result)
   }
 );
