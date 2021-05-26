63514d9efe8ac4c68bf63c52a09f62cb58dd4283
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-26 18:00:28 -0300

    added database_url env

--- Server/Index.ts
+++ Server/Index.ts
@@ -1,15 +1,25 @@
 import { Sequelize } from "sequelize-typescript";
 
-const { DB_NAME, DB_PASSWORD, DB_USERNAME, DB_PORT } = process.env as any;
+const {
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USERNAME,
+  DB_PORT,
+  DATABASE_URL
+} = process.env as any;
 
-const sequelize = new Sequelize({
-  database: DB_NAME,
-  dialect: "postgres",
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  models: [__dirname + "/Models"],
-  logging: false
-});
+const sequelize = new Sequelize(
+  DATABASE_URL
+    ? DATABASE_URL
+    : {
+        database: DB_NAME,
+        dialect: "postgres",
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        port: DB_PORT,
+        models: [__dirname + "/Models"],
+        logging: false
+      }
+);
 
 export default sequelize;
