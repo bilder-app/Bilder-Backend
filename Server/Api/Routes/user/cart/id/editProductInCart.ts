f7d7f0d2129ebca778e7f22c98dc10fa5a2dcf42
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-08 17:51:36 -0300

    created

---
+++ Server/Api/Routes/user/cart/id/editProductInCart.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import ProductInCart from "../../../../../Models/ProductInCart";
+import isPerson from "../../../../middleware/isPerson";
+
+const ROUTE = "/user/cart/:productId";
+
+export default Router({ mergeParams: true }).put(
+  ROUTE,
+  isPerson,
+  async (req, res) => {
+    const { productId } = req.params as unknown as { productId: number };
+    const { amount } = req.body;
+    await ProductInCart.update(
+      {
+        amount
+      },
+      {
+        where: {
+          personId: req.user!.id,
+          productId
+        },
+        returning: true
+      }
+    );
+    res.sendStatus(200);
+  }
+);
