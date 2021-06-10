54c33fa672be3609efd4bc0f88cdec4704f265e3
Lucasmercado101 <lucasmercado101@gmail.com>
2021-06-10 14:22:47 -0300

    fixed

--- Server/Api/Routes/products/search/paginatedSearchProducts.ts
+++ Server/Api/Routes/products/search/paginatedSearchProducts.ts
@@ -5,11 +5,11 @@ import { Op } from "sequelize";
 const ROUTE = "/products/search";
 
 export default Router({ mergeParams: true }).get(ROUTE, async (req, res) => {
-  const { name, page, limit = 50 } = (req.params as unknown) as {
-    name: string;
-    page: number;
-    limit: number;
-  };
+  const { name, limit: limitStr, page: pageStr } = req.query;
+  if (!limitStr || !pageStr) return res.sendStatus(400);
+  const limit = parseInt(limitStr as string);
+  const page = parseInt(pageStr as string);
+
   const productsAmount = await Product.count({
     where: { name: { [Op.iLike]: `%${name}%` } }
   });
@@ -20,37 +20,33 @@ export default Router({ mergeParams: true }).get(ROUTE, async (req, res) => {
   let next: undefined | object = undefined;
   let previous: undefined | object = undefined;
 
-  if (startIndex > 0) {
-    previous = {
-      page: page - 1,
-      limit
-    };
-  }
-
   if (endIndex < productsAmount) {
     next = {
       page: page + 1,
-      limit
+      limit: limit
     };
   }
 
-  const totalPaginationPages =
-    productsAmount % limit === 0
-      ? productsAmount / limit
-      : ~~(productsAmount / limit) + 1;
+  if (startIndex > 0 && startIndex < productsAmount) {
+    previous = {
+      page: page - 1,
+      limit: limit
+    };
+  }
 
-  return Product.findAndCountAll({
+  const products = await Product.findAll({
     where: {
       name: { [Op.iLike]: `%${name}%` }
     },
     order: [["name", "ASC"]],
     limit,
     offset: Math.max(0, startIndex)
-  }).then((resp) => ({
-    totalProducts: productsAmount,
-    totalPaginationPages,
+  });
+
+  res.json({
+    total: productsAmount,
     next,
     previous,
-    products: resp.rows
-  }));
+    results: products
+  });
 });
