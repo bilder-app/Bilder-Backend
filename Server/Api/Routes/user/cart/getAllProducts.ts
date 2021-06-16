410f629c54275297dc5ea2aacc334d89e16ca2e8
Lucasmercado101 <lucasmercado101@gmail.com>
2021-06-16 19:12:26 -0300

    changed

--- Server/Api/Routes/user/cart/getAllProducts.ts
+++ Server/Api/Routes/user/cart/getAllProducts.ts
@@ -6,5 +6,16 @@ const ROUTE = "/user/cart";
 export default Router({ mergeParams: true }).get(
   ROUTE,
   isPerson,
-  async (req, res) => res.json(await req.person!.$get("cartProducts"))
+  async (req, res) => {
+    const products = await req
+      .person!.$get("cartProducts")
+      .then((resp) => JSON.parse(JSON.stringify(resp)));
+
+    const resp = products.map((prod: any) => {
+      prod.amount = prod.ProductInCart.amount;
+      delete prod.ProductInCart;
+      return prod;
+    });
+    res.json(resp);
+  }
 );
