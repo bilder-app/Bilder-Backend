5ddde35d64a24b82a01780382b14b325832c8e32
DieG02 <43993554@alejandrovolta.edu.ar>
2021-06-18 21:01:07 -0300

    edit Subcategories

--- Server/Api/Routes/business/products/id/editProduct.ts
+++ Server/Api/Routes/business/products/id/editProduct.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import isBusiness from "../../../../middleware/isBusiness";
+import ProductSubCategory from "../../../../../Models/ProductSubCategory";
 
 const ROUTE = "/business/products/:productId";
 
@@ -8,11 +9,26 @@ export default Router({ mergeParams: true }).put(
   isBusiness,
   async (req, res) => {
     const { productId } = req.params;
+    const { subcategories } = req.body;
     const [product] = await req.business!.$get("products", {
       where: { id: productId }
     });
     if (!product) return res.sendStatus(404);
-    const updatedProduct = await product.update(req.body, { returning: true });
-    res.json(updatedProduct);
+    
+    const [ response, created ] = await ProductSubCategory.findOrCreate({
+      where: { productId: product.id },
+      defaults: { subcategory: subcategories }
+    })
+    if(!created) {
+      await ProductSubCategory.update(
+        { subcategory: subcategories },
+        {
+          where: { productId: product.id }
+        }
+      )
+    }
+
+    await product.update(req.body, { returning: true });
+    res.json(200);
   }
 );
