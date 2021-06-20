504cdabb9d1501eb576d362740b752bb81ff2f59
billybautista <soybilder@gmail.com>
2021-06-20 15:01:48 -0300

    Fix subcategories

--- Server/Api/Routes/categories/id/getSubcategoriesFromProduct.ts
+++ Server/Api/Routes/categories/id/getSubcategoriesFromProduct.ts
@@ -4,20 +4,15 @@ import ProductSubCategory from "../../../../Models/ProductSubCategory";
 
 const ROUTE = "/categories/:productId";
 
-export default Router({ mergeParams: true }).get(
-  ROUTE, 
-  async (req, res) => {
+export default Router({ mergeParams: true }).get(ROUTE, async (req, res) => {
   const { productId } = req.params;
-  
-  try {
-    const resp = await ProductSubCategory.findOne({
-      where: { productId: productId }
-    })
-    res.json( await SubCategory.findByPk(resp.subcategory) )
-  }
-  catch {
-    res.status(404).send({ message: "No hay categorias disponibles" })
-  }
 
-
+  const resp = await ProductSubCategory.findOne({
+    where: { productId: productId },
+  });
+  if (!!resp) {
+    res.json(await SubCategory.findByPk(resp.subcategory));
+  } else {
+    res.status(404).send({ message: "No hay categorias disponibles" });
+  }
 });
